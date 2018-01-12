877f0614ce6c1191d03d69f3837f6b3ebbd4feea
Ethan Lee <leeethan186@gmail.com>
2018-01-12 14:41:48 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,8 @@
 module.exports = {
-  consumer_key: 'YOUR CONSUMER KEY',
-  consumer_secret: 'YOUR SECRET KEY',
-  access_token_key: 'YOUR ACCESS TOKEN KEY',
-  access_token_secret: 'YOUR ACCESS TOKEN SECRET'
+  consumer_key: 'B7mPdVC3yWScPWOV6q7916KJv',
+  consumer_secret: 'MJxdsMWipiqCt1hRMnkhtw6pyNcjzv2BuwYQGG7ypAC6FMVFJU
+',
+  access_token_key: '	905431923090235397-ZptBqTstppElP8uLBLz7ZkOwLL6OlIJ',
+  access_token_secret: '	pvTX5G0CK4fH05HPKoT2Gv5Ep1cBKPExW9kZmZrPMyIOb
+'
 }
